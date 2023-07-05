334c64e6e45d7a2164bd7015366efda0d87cbc7e
Raju Rangan <rrkasthu@amazon.com>
2023-07-05 00:34:00 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# End-to-End Machine Learning using ray
+
+This workshop goes through a common customer journey when beginning and maturing their MLOps journey from Initial to Scalable. 
+
+![MLOPs stages of adoption](images/maturity-model-1.png)
+
+This workshop will introduce the use of [Ray](https://docs.ray.io/en/latest/ray-overview/getting-started.html) on Sagemaker. Ray is an open-source distributed computing framework designed to accelerate and simplify the development of scalable and efficient machine learning applications. It provides a simple and flexible API for defining and executing [tasks](https://docs.ray.io/en/latest/ray-core/tasks.html) and [actors](https://docs.ray.io/en/latest/ray-core/actors.html) on a cluster of machines, allowing you to easily scale your machine learning workloads from a single machine to thousands of nodes.
+
+Ray is designed to [support a wide range of machine learning libraries and frameworks](https://docs.ray.io/en/latest/train/getting-started.html), including popular tools like TensorFlow, PyTorch, and scikit-learn. It also includes built-in support for distributed reinforcement learning, hyperparameter tuning, and model serving, making it easy to build end-to-end machine learning pipelines.
+
+One of the key features of Ray is its ability to handle both task parallelism and actor-based concurrency, allowing you to easily express complex computation patterns and data dependencies. Ray also provides efficient scheduling and data sharing mechanisms, enabling fast and scalable execution of machine learning workloads.
+
+Ray is widely used in both industry and academia, and has a large and active community of contributors and users. Whether you’re building a large-scale machine learning system or just getting started with distributed computing, Ray provides a powerful and easy-to-use platform for building and running your machine learning applications.
+
+## Lab Introduction
+
+Please refer to the following link for detailed workshop guidance: [SageMaker MLOps Workshop](https://bit.ly/40txRE6)
+
+
+## Additional Resources
+
+* Check this [Blog Post](https://aws.amazon.com/blogs/machine-learning/mlops-foundation-roadmap-for-enterprises-with-amazon-sagemaker/)
+for further details on the MLOps Foundations Roadmap for Enterprises on Amazon SageMaker.
+
+* [SageMaker MLOps Custom Project Templates GitHub Repository](https://github.com/aws-samples/sagemaker-custom-project-templates): This repository contains example projects templates based on common customer patterns. 
---
+++ pipeline_scripts/preprocessing/script.py
@@ -0,0 +1,138 @@
+import subprocess
+import sys
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'sagemaker','ray'])
+
+import argparse
+import os
+
+import ray
+from ray.air.config import ScalingConfig
+from ray.data import Dataset
+from ray.air.result import Result
+from ray.data.preprocessors import StandardScaler
+
+def read_parameters():
+    """
+    Read job parameters
+    Returns:
+        (Namespace): read parameters
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_size', type=float, default=0.6)
+    parser.add_argument('--val_size', type=float, default=0.2)
+    parser.add_argument('--test_size', type=float, default=0.2)
+    parser.add_argument('--random_state', type=int, default=42)
+    parser.add_argument('--target_col', type=str, default='PRICE')
+    params, _ = parser.parse_known_args()
+    return params
+
+def split_dataset(dataset, train_size, val_size, test_size, random_state=None):
+    """
+    Split dataset into train, validation and test samples
+    Args:
+        dataset (ray.data.Dataset): input data
+        train_size (float): ratio of data to use as training dataset
+        val_size (float): ratio of data to use as validation dataset
+        test_size (float): ratio of data to use as test dataset
+        random_state (int): Pass an int for reproducible output across multiple function calls.
+    Returns:
+        train_set (ray.data.Dataset): train dataset
+        val_set (ray.data.Dataset): validation dataset
+        test_set (ray.data.Dataset): test dataset
+    """
+    if (train_size + val_size + test_size) != 1.0:
+        raise ValueError("train_size, val_size and test_size must sum up to 1.0")
+    
+    # Shuffle this dataset with a fixed random seed.
+    shuffled_ds = dataset.random_shuffle(seed=random_state)
+    # Split the data into train, validation and test datasets
+    train_set, val_set, test_set = shuffled_ds.split_proportionately([train_size, val_size])
+    
+    # Sanity check
+    # IMPORTANT!!! Do not include this for large datasets as this can be an expensive operation
+    train_perc = int((train_set.count()/shuffled_ds.count()) * 100)
+    print(f"Training size: {train_set.count()} - {train_perc}% of total")
+    val_perc = int((val_set.count()/shuffled_ds.count()) * 100)
+    print(f"Val size: {val_set.count()} - {val_perc}% of total")
+    test_perc = int((test_set.count()/shuffled_ds.count()) * 100)
+    print(f"Test size: {test_set.count()} - {test_perc}% of total")
+    return train_set, val_set, test_set
+
+def scale_dataset(train_set, val_set, test_set, target_col):
+    """
+    Fit StandardScaler to df_train and apply it to df_val and df_test
+    Args:
+        train_set (ray.data.Dataset): train dataset
+        val_set (ray.data.Dataset): validation dataset
+        test_set (ray.data.Dataset): test dataset
+        target_col (str): target col
+    Returns:
+        train_transformed (ray.data.Dataset): train data scaled
+        val_transformed (ray.data.Dataset): val data scaled
+        test_transformed (ray.data.Dataset): test data scaled
+    """
+    
+    tranform_cols = dataset.columns()
+    # Remove the target columns from being scaled
+    tranform_cols.remove(target_col)
+    # set up a standard scaler
+    standard_scaler = StandardScaler(tranform_cols)
+    # fit scaler to training dataset
+    print("Fitting scaling to training data and transforming dataset...")
+    train_set_transformed = standard_scaler.fit_transform(train_set)
+    # apply scaler to validation and test datasets
+    print("Transforming validation and test datasets...")
+    val_set_transformed = standard_scaler.transform(val_set)
+    test_set_transformed = standard_scaler.transform(test_set)
+    return train_set_transformed, val_set_transformed, test_set_transformed
+
+
+print(f"===========================================================")
+print(f"Starting pre-processing")
+print(f"Reading parameters")
+
+# reading job parameters
+args = read_parameters()
+print(f"Parameters read: {args}")
+
+# set input and output paths
+input_data_path = "/opt/ml/processing/input/house_pricing.csv"
+train_data_path = "/opt/ml/processing/output/train"
+val_data_path = "/opt/ml/processing/output/validation"
+test_data_path = "/opt/ml/processing/output/test"
+
+try:
+    os.makedirs(train_data_path)
+    os.makedirs(val_data_path)
+    os.makedirs(test_data_path)
+except:
+    pass
+
+
+# read data input
+dataset = ray.data.read_csv(input_data_path)
+
+# split dataset into train, validation and test
+train_set, val_set, test_set = split_dataset(
+    dataset,
+    train_size=args.train_size,
+    val_size=args.val_size,
+    test_size=args.test_size,
+    random_state=args.random_state
+)
+
+
+# scale datasets
+train_transformed, val_transformed, test_transformed = scale_dataset(
+    train_set, 
+    val_set, 
+    test_set,
+    args.target_col
+)
+
+print("Saving data")
+train_transformed.write_csv(train_data_path)
+val_transformed.write_csv(val_data_path)
+test_transformed.write_csv(test_data_path)
+print(f"Ending pre-processing")
+print(f"===========================================================")
