99b85f16906204ee246acd54cdb1d4a2ec57ce04
rrangan <rrangan@gmail.com>
2023-07-21 08:38:44 +0000

    Changed the training to consume data from the data prep step.

--- pipeline_scripts/train/script.py
+++ pipeline_scripts/train/script.py
@@ -1,20 +1,25 @@
 import subprocess
 import sys
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'sagemaker','ray', 'xgboost_ray', 'pyarrow >= 6.0.1'])
+# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas==1.5.2', 'sagemaker','ray[all]==2.4.0', 'modin[ray]==0.18.0', 'xgboost_ray', 'pyarrow >= 6.0.1','pydantic==1.10.10', 'gpustat==1.0.0'])
+
 import os
 import time
-import ray.cloudpickle as cloudpickle
+from glob import glob
 import argparse
 import json
 import logging
 import boto3
 import sagemaker
+import numpy as np
+import modin.pandas as pd
+
 # Experiments
 from sagemaker.session import Session
 from sagemaker.experiments.run import load_run
 
 import ray
-from ray.train.xgboost import XGBoostTrainer
+from xgboost_ray import RayDMatrix, RayParams, train
+
 from ray.air.config import ScalingConfig
 from ray.data import Dataset
 from ray.air.result import Result
@@ -29,10 +34,10 @@ def read_parameters():
     parser = argparse.ArgumentParser()
 
     # Hyperparameters are described here.
-    parser.add_argument('--max_depth', type=int)
-    parser.add_argument('--eta', type=float)
-    parser.add_argument('--min_child_weight', type=int)
-    parser.add_argument('--subsample', type=float)
+    parser.add_argument('--max_depth', type=int, default=os.environ.get('SM_HP_MAX_DEPTH'))
+    parser.add_argument('--eta', type=float, default=os.environ.get('SM_HP_ETA'))
+    parser.add_argument('--min_child_weight', type=int, default=os.environ.get('SM_HP_MIN_CHILD_WEIGHT'))
+    parser.add_argument('--subsample', type=float, default=os.environ.get('SM_HP_SUBSAMPLE'))
     parser.add_argument('--verbosity', type=int)
     parser.add_argument('--num_round', type=int)
     parser.add_argument('--tree_method', type=str, default="auto")
@@ -46,7 +51,7 @@ def read_parameters():
     parser.add_argument('--sm_hosts', type=str, default=os.environ.get('SM_HOSTS'))
     parser.add_argument('--sm_current_host', type=str, default=os.environ.get('SM_CURRENT_HOST'))
     
-    parser.add_argument('--num_ray_workers', type=int,default=3)
+    parser.add_argument('--num_ray_workers', type=int,default=6)
     parser.add_argument('--use_gpu', type=bool, default=False)
     # parse region
     parser.add_argument('--region', type=str, default='us-east-1')
@@ -63,7 +68,7 @@ def read_parameters():
     args, _ = parser.parse_known_args()
     return args
 
-def load_dataset(fs_data_loc, target_col="price"):
+def load_dataset(path, num_workers, target_col="price"):
     """
     Loads the data as a ray dataset from the offline featurestore S3 location
     Args:
@@ -72,24 +77,16 @@ def load_dataset(fs_data_loc, target_col="price"):
     Returns:
         ds (ray.data.dataset): Ray dataset the contains the requested dat from the feature store
     """
-    # Drop columns added by the feature store
-    cols_to_drop = ["record_id", "event_time","write_time", 
-                    "api_invocation_time", "is_deleted", 
-                    "year", "month", "day", "hour"]
-                    
-    
-    # A simple check is this is test data
-    # If True add the target column to the columns list to be dropped
-    if '/test/' in fs_data_loc:
-        cols_to_drop.append(target_col)
+ 
+    csv_files = glob(os.path.join(path, "*.csv"))
+    print(f"found {len(csv_files)} files at {path}")
+    ds = ray.data.read_csv(path)
+    # ds = ds.drop_columns(cols_to_drop)
+    print(f"{path} count is {ds.count()}")
 
-    ds = ray.data.read_parquet(fs_data_loc)
-    ds = ds.drop_columns(cols_to_drop)
-    print(f"{fs_data_loc} count is {ds.count()}")
+    return ds.repartition(num_workers)
 
-    return ds
-
-def train_xgboost(ds_train, ds_val, params, num_workers, use_gpu = False, target_col = "price") -> Result:
+def train_xgboost(ds_train, ds_val, params, num_workers, target_col = "price") -> Result:
     """
     Creates a XGBoost trainer, train it, and return the result.        
     Args:
@@ -97,24 +94,45 @@ def train_xgboost(ds_train, ds_val, params, num_workers, use_gpu = False, target
         ds_val (ray.data.dataset): Validation dataset
         params (dict): Hyperparameters
         num_workers (int): number of workers to distribute the training across
-        use_gpu (bool): Should the taining job use GPUs
         target_col (str): target column
     Returns:
         result (ray.air.result.Result): Result of the training job
     """
-    trainer = XGBoostTrainer(
-        scaling_config=ScalingConfig(num_workers=num_workers, use_gpu=use_gpu),
-        label_column="PRICE",
+    
+    train_set = RayDMatrix(ds_train, 'PRICE')
+    val_set = RayDMatrix(ds_val, 'PRICE')
+    
+    evals_result = {}
+    
+    trainer = train(
         params=params,
-        datasets={"train": ds_train, "valid": ds_val},
+        dtrain=train_set,
+        evals_result=evals_result,
+        evals=[(val_set, "validation")],
+        verbose_eval=False,
         num_boost_round=100,
+        ray_params=RayParams(num_actors=num_workers, cpus_per_actor=1),
     )
-    result = trainer.fit()
-    print("<==== Start Training Metrics ====>")
-    print(result.metrics)
-    print("<==== END Training Metrics ====>")
-
-    return result
+    
+    output_path=os.path.join(args.model_dir, 'model.xgb')
+    
+    trainer.save_model(output_path)
+    
+    valMAE = evals_result["validation"]["mae"][-1]
+    valRMSE = evals_result["validation"]["rmse"][-1]
+ 
+    print('[3] #011validation-mae:{}'.format(valMAE))
+    print('[4] #011validation-rmse:{}'.format(valRMSE))
+    
+    local_testing = False
+    try:
+        load_run(sagemaker_session=sess)
+    except:
+        local_testing = True
+    if not local_testing: # Track experiment if using SageMaker Training
+        with load_run(sagemaker_session=sess) as run:
+            run.log_metric('validation-mae', valMAE)
+            run.log_metric('validation-rmse', valRMSE)
 
 def main():
     # Get SageMaker host information from runtime environment variables
@@ -133,41 +151,11 @@ def main():
         "seed": 47
     }
 
-    ds_train = load_dataset(args.train, args.target_col)
-    ds_validation = load_dataset(args.validation, args.target_col)
+    ds_train = load_dataset(args.train, args.num_ray_workers, args.target_col)
+    ds_validation = load_dataset(args.validation, args.num_ray_workers, args.target_col)
     
-    result = train_xgboost(ds_train, ds_validation, hyperparams, args.num_ray_workers, args.use_gpu, args.target_col)
-    metrics = result.metrics
-    # checkpoint = result.checkpoint.to_directory(path=os.path.join(args.model_dir, f'model.xgb'))
-    
-    output_path=os.path.join(args.model_dir, f'model.pkl')
-    # Serialize the trained model using ray.cloudpickle
-    serialized_model = cloudpickle.dumps(result)
+    trainer = train_xgboost(ds_train, ds_validation, hyperparams, args.num_ray_workers, args.target_col)
 
-    # Save the serialized model to a file
-    with open(output_path, 'wb') as f:
-        f.write(serialized_model)
-    
-    trainMAE = metrics['train-mae']
-    trainRMSE = metrics['train-rmse']
-    valMAE = metrics['valid-mae']
-    valRMSE = metrics['valid-rmse']
-    print('[1] #011train-mae:{}'.format(trainMAE))
-    print('[2] #011train-rmse:{}'.format(trainRMSE))
-    print('[3] #011validation-mae:{}'.format(valMAE))
-    print('[4] #011validation-rmse:{}'.format(valRMSE))
-    
-    local_testing = False
-    try:
-        load_run(sagemaker_session=sess)
-    except:
-        local_testing = True
-    if not local_testing: # Track experiment if using SageMaker Training
-        with load_run(sagemaker_session=sess) as run:
-            run.log_metric('train-mae', trainMAE)
-            run.log_metric('train-rmse', trainRMSE)
-            run.log_metric('validation-mae', valMAE)
-            run.log_metric('validation-rmse', valRMSE)
     
 if __name__ == '__main__':
     ray_helper = RayHelper()
